35dd6439f8e68a8efffa5751444a86966120e642
Ethan McGee <ethanmcgee@EthanMcGeesMBP.localdomain>
2024-01-15 10:38:57 -0500

    okay we are ready to write

--- formbox/models.py
+++ formbox/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 PROTECTION_TYPE = [
     ('NONE', 'None'),
@@ -19,10 +20,48 @@ DAY_PART = [
     ('PM', 'PM')
 ]
 
+TWO_FACTOR_TYPE = [
+    ('TOTP', 'Time-Based One Time Password'),
+    ('SMS', 'SMS'),
+    ('EMAIL', 'E-mail'),
+]
+
+
+class AuthSetting(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    password_reset_token = models.CharField(max_length=255, null=True)
+    two_factor_auth_token = models.CharField(max_length=255, null=True)
+    invite_token = models.CharField(max_length=255, null=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_auth_setting(sender, instance, created, **kwargs):
+    if created:
+        AuthSetting.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_auth_setting(sender, instance, **kwargs):
+    instance.auth_setting.save()
+
+
+class TwoFactorOption(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    two_factor_type = models.CharField(max_length=6, choices=TWO_FACTOR_TYPE)
+    target = models.CharField(max_length=255, null=True)
+    secret = models.CharField(max_length=255, null=True)
+    nickname = models.CharField(max_length=255, null=True)
+
 
 class Team(models.Model):
     name = models.CharField(max_length=255)
     users = models.ManyToManyField(User)
+
+
+class TeamInvite(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    invitee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='invitee_id')
+    invited_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='invited_by_id')
 
 
 class Project(models.Model):
